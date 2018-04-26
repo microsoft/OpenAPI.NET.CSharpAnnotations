49ff216229dc7c97b87a78fe20164a184b2464d5
Shweta Patil <shwetap@microsoft.com>
2018-04-26 11:38:26 -0700

    - Sign assemblies using snk - Rev the newtonsoft version, as 4.0.8 is not compatible with .net standard

--- src/Microsoft.OpenApi.CSharpAnnotations.NewtonsoftJsonExtensions/Properties/AssemblyInfo.cs
+++ src/Microsoft.OpenApi.CSharpAnnotations.NewtonsoftJsonExtensions/Properties/AssemblyInfo.cs
@@ -5,4 +5,6 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Microsoft.OpenApi.CSharpAnnotations.NewtonsoftJsonExtensions.Tests")]
+[assembly:
+    InternalsVisibleTo(
+        "Microsoft.OpenApi.CSharpAnnotations.NewtonsoftJsonExtensions.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000d5363da7d3a2c3eb8403c418b17a8911940293ee0324f02ddcf17afe1a6f79c05d5434f5b0b3634bbe37181a59cd462a9baeafb8936ae68e58ea0b6e7d792e452c505b0c01594c473c4e7aa6e14de55cb8a4d316caea03f908ff21d04eb9efc0ca0ff502f3009f151d7d7a342e400e9fc0d7bca9afe61494fe8fe83828309cf" )]
